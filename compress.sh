fa7480fd68b687e8e9bf8f16dbd2b88b6ae418ab
warrenc5 <warren@mofokom.biz>
2022-02-06 08:08:22 +1100

    add option for local conversion

--- compress.sh
+++ compress.sh
@@ -3,10 +3,12 @@
 if [ -f /openrc.sh ] ; then
   echo 'sourcing openrc.sh'
   . /openrc.sh
+  GLANCE=1
 fi 
 
-#BASE=/pub
-OLD=$BASE/in.$INPUT_FORMAT
+BASE=/pub
+#OLD=$BASE/$INFILE.$INPUT_FORMAT
+OLD=$BASE/$INFILE
 NEW=$BASE/out.$OUTPUT_FORMAT
 echo ${SRC} $NAME
 
@@ -19,11 +21,20 @@ echo ${SRC} $NAME
 #INPUT_FORMAT=qcow2
 cd $BASE
 
+if [ ! -z "$GLANCE" ] ; then
 echo "downloading."
 glance image-download --progress --file $OLD $SRC
 echo "downloaded."
 
+if [ ! -f $OLD ] ; then echo "download failed" && exit 1 
 ls -lah $DEST
+fi 
+else 
+
+echo "skipping download - no glance info"
+
+fi 
+
 
 echo "converting."
 qemu-img convert -p -c -f $INPUT_FORMAT -O $OUTPUT_FORMAT -o compat=$COMPAT $OLD $NEW
@@ -31,7 +42,17 @@ qemu-img convert -p -c -f $INPUT_FORMAT -O $OUTPUT_FORMAT -o compat=$COMPAT $OLD
 echo "converted."
 ls -lah $NEW
 
+if [ ! -f $NEW ] ; then echo "compress failed" && exit 1 
+
+if [ ! -z "$GLANCE" ] ; then
+
 echo "uploading."
 openstack image create --disk-format $OUTPUT_FORMAT --container-format bare --file $NEW "$NAME" "--private" --property distribution=$DISTRO --property hw_disk_bus=scsi --property hw_scsi_model=virtio-scsi --property hw_qemu_guest_agent=yes --property image_original_user=$IMG_ORIG_USER
+
+else 
+
+echo "skipping upload - no glance info"
+
+fi 
 
 echo "done."
